d1b84e63af7b0f9b7291225450b5d70c8a30b702
PUYGRENIER Solann <puygrenier.solann@gmx.fr>
2020-10-04 11:49:16 +0200

    Send file for the TP2

---
+++ TP2_PUYGRENIER-Solann/ArmurerieException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Puygrenier_Solann
+{
+    public class ArmurerieException : System.Exception{
+        public ArmurerieException() : base(){}
+        public ArmurerieException(string message) : base(message) { }
+    }
+}
---
+++ TP2_PUYGRENIER-Solann/Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Puygrenier_Solann
+{
+
+    public enum Type { Direct, Explosife, AutoGuide, }
+
+    public class Weapon{
+        private const int MINIMUM_DAMAGE = 10;
+        private const int MAXIMUM_DAMAGE = 100;
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public Type WeaponType { get; set; }
+
+        /// <summary>
+        /// Default Weapon
+        /// </summary>
+        public Weapon(){
+            this.Name = "Crycket";
+            this.Damage = MINIMUM_DAMAGE;
+            this.WeaponType = Type.Direct;
+        }
+
+        public Weapon(string name, int damage, Type weaponType){
+            Name = name;
+            Damage = damage;
+            WeaponType = weaponType;
+        }
+
+        public override string ToString(){
+            return Name.ToString() +"/"+ Damage.ToString()+"dmg/"+ WeaponType.ToString();
+        }
+
+    }
+
+     
+}
